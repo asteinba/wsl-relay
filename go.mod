f57e83dc38d1003886f65d1f9098651b5841a341
Angelo Steinbach <asteinba@stone-dev.de>
2023-09-14 04:00:43 +0200

    Removed anything that is not relevant for openssh relaying, renamed the project and updated deps, updated README.

--- go.mod
+++ go.mod
@@ -1,9 +1,13 @@
-module github.com/Lexicality/wsl-relay
+module github.com/asteinba/openssh-agent-wsl-relay
 
-go 1.19
+go 1.18
 
 require (
-	github.com/Microsoft/go-winio v0.5.2
-	github.com/spf13/pflag v1.0.5
-	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261
+	github.com/Microsoft/go-winio v0.6.1
+	golang.org/x/sys v0.12.0
+)
+
+require (
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,28 +1,12 @@
 package main
 
 import (
+	"github.com/Microsoft/go-winio"
+	"golang.org/x/sys/windows"
 	"io"
 	"log"
 	"os"
-	"path/filepath"
 	"sync"
-	"time"
-
-	flag "github.com/spf13/pflag"
-	"golang.org/x/sys/windows"
-)
-
-// How long to sleep between failures while polling
-const pollTimeout = 200 * time.Millisecond
-
-var (
-	poll            = flag.BoolP("poll", "p", false, "poll until the the specified thing exists")
-	closeWrite      = flag.BoolP("close-pipe", "s", false, "close the write channel after stdin closes")
-	closeOnEOF      = flag.Bool("pipe-closes", false, "terminate when pipe closes, regardless of stdin state")
-	closeOnStdinEOF = flag.Bool("input-closes", false, "terminate on stdin closes, regardless of pipe state")
-	verbose         = flag.BoolP("verbose", "v", false, "verbose output on stderr")
-	namedPipe       = flag.String("pipe", "", "The name of the pipe you wish to connect to")
-	gpgFile         = flag.String("gpg", "", "To location of your windows GPG agent socket")
 )
 
 func underlyingError(err error) error {
@@ -32,58 +16,14 @@ func underlyingError(err error) error {
 	return err
 }
 
-type closeWriter interface {
-	CloseWrite() error
-}
-
 func main() {
-	flag.Lookup("gpg").NoOptDefVal = "S.gpg-agent"
-
-	flag.Parse()
-
 	var conn io.ReadWriteCloser
-	var err error
 
-	if *namedPipe != "" {
-		if *verbose {
-			log.Println("Creating a pipe to", *namedPipe)
-		}
-		conn, err = dialPipe(*namedPipe, *poll)
-	} else if *gpgFile != "" {
-		fileName := *gpgFile
-		if !filepath.IsAbs(fileName) {
-			localAppData, ok := os.LookupEnv("LOCALAPPDATA")
-			if !ok {
-				log.Fatal("Missing the %LOCALAPPDATA% variable?")
-			}
-			gpgDir := filepath.Join(localAppData, "gnupg")
-			_, err := os.Stat(gpgDir)
-			if os.IsNotExist(err) {
-				log.Fatalf("The directory %q doesn't exist, please specify your full GPG path", gpgDir)
-			}
-
-			fileName = filepath.Join(gpgDir, fileName)
-		}
-
-		if *verbose {
-			log.Println("Opening an Assuan connection via", fileName)
-		}
-
-		conn, err = dialAssuan(fileName, *poll)
-	} else {
-		log.Fatalln("No action specified!")
-		flag.Usage()
-		os.Exit(1)
-	}
-
+	conn, err := winio.DialPipe("//./pipe/openssh-ssh-agent", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer conn.Close()
-
-	if *verbose {
-		log.Println("connected")
-	}
 
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -93,25 +33,8 @@ func main() {
 			log.Fatalln("copy from stdin to pipe failed:", err)
 		}
 
-		if *verbose {
-			log.Println("copy from stdin to pipe finished")
-		}
+		os.Exit(0)
 
-		if *closeOnStdinEOF {
-			os.Exit(0)
-		}
-
-		if *closeWrite {
-			cw, ok := conn.(closeWriter)
-			if ok {
-				err = cw.CloseWrite()
-				if err != nil {
-					if *verbose {
-						log.Println("Failed to close pipe:", err)
-					}
-				}
-			}
-		}
 		os.Stdin.Close()
 		wg.Done()
 	}()
@@ -122,39 +45,11 @@ func main() {
 		// named pipes are not bidirectional, there is no way for the other side
 		// of the pipe to get more data, so do not wait for the stdin copy to
 		// finish.
-		if *verbose {
-			log.Println("copy from pipe to stdout finished: pipe closed")
-		}
+		log.Println("copy from pipe to stdout finished: pipe closed")
 		os.Exit(0)
 	}
 
 	if err != nil {
 		log.Fatalln("copy from pipe to stdout failed:", err)
-	}
-
-	if *verbose {
-		log.Println("copy from pipe to stdout finished")
-	}
-
-	if !*closeOnEOF {
-		os.Stdout.Close()
-
-		// Keep reading until we get ERROR_BROKEN_PIPE or the copy from stdin
-		// finishes.
-		go func() {
-			for {
-				_, err := conn.Read(nil)
-				if underlyingError(err) == windows.ERROR_BROKEN_PIPE {
-					if *verbose {
-						log.Println("pipe closed")
-					}
-					os.Exit(0)
-				} else if err != nil {
-					log.Fatalln("pipe error:", err)
-				}
-			}
-		}()
-
-		wg.Wait()
 	}
 }
